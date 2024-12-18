89136b11f758b9bae3b5d723a207c465935bacdd
Erhan Delen <Erhandln611@gmail.com>
2024-12-18 17:38:44 +0300

    hava durumu projesi

---
+++ src/main/java/com/foldsdev/weather/dto/WeatherResponse.java
@@ -0,0 +1,8 @@
+package com.foldsdev.weather.dto;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record WeatherResponse(
+        Request request,
+        Location location,
+        Current current) {
+}
